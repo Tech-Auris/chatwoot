d9e0d4f554c57a63c1fdd4b2a3eb8fa38bbb4bb6
Tech-Auris <tech@agenteauris.com.br>
2026-09-21 21:21:51 -0300

    chore(media-hub): bump the check icon from 14 to 18 so it fills the box

--- app/javascript/dashboard/routes/dashboard/media/Index.vue
+++ app/javascript/dashboard/routes/dashboard/media/Index.vue
@@ -433,8 +433,8 @@ const runMenuAction = mi => {
                   <svg
                     v-if="isSelected(item.id)"
                     xmlns="http://www.w3.org/2000/svg"
-                    width="14"
-                    height="14"
+                    width="18"
+                    height="18"
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="#111827"
@@ -530,8 +530,8 @@ const runMenuAction = mi => {
                       <svg
                         v-if="isSelected(item.id)"
                         xmlns="http://www.w3.org/2000/svg"
-                        width="14"
-                        height="14"
+                        width="18"
+                        height="18"
                         viewBox="0 0 24 24"
                         fill="none"
                         stroke="#ffffff"
